27b7f41c438f502d3d4d57c199e18070d468ddbe
tkornilova <kornilova.tat07@gmail.com>
2022-07-13 20:52:14 +0400

    Исправление багов

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,5 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
-import './modules/menu.js';
 
 // ---------------------------------
 
@@ -45,3 +44,45 @@ window.addEventListener('DOMContentLoaded', () => {
 // breakpointChecker();
 
 // используйте .closest(el)
+
+// ---------------------------------
+
+let buttonOpen = document.querySelector('.page-header__button-open');
+let buttonClose = document.querySelector('.page-header__button-close');
+let pageHeader = document.querySelector('.page-header');
+
+if (pageHeader.classList.contains('page-header-no-js')) {
+  pageHeader.classList.remove('page-header-no-js');
+  pageHeader.classList.add('page-header--closed');
+  buttonOpen.classList.remove('visually-hidden');
+  buttonClose.classList.add('visually-hidden');
+}
+
+buttonOpen.addEventListener('click', function () {
+  buttonOpen.classList.add('visually-hidden');
+  buttonClose.classList.remove('visually-hidden');
+  pageHeader.classList.remove('page-header--closed');
+  pageHeader.classList.add('page-header--opened');
+});
+
+buttonClose.addEventListener('click', function () {
+  buttonClose.classList.add('visually-hidden');
+  buttonOpen.classList.remove('visually-hidden');
+  pageHeader.classList.remove('page-header--opened');
+  pageHeader.classList.add('page-header--closed');
+});
+
+const form = document.querySelector('.booking__form');
+const nameInput = form.querySelector('.booking__form-name');
+const phoneInput = form.querySelector('.booking__form-phone');
+const emailInput = form.querySelector('.booking__form-email');
+
+form.addEventListener('submit', (evt) => {
+  if (!nameInput.value || !phoneInput.value || !emailInput.value) {
+    evt.preventDefault();
+  } else {
+    localStorage.setItem('name', nameInput.value);
+    localStorage.setItem('phone', phoneInput.value);
+    localStorage.setItem('email', emailInput.value);
+  }
+});
